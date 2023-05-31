72fdbaf11b0ba9f439a1e47eee869723406fa984
Tilen Marš <its0black0magic@gmail.com>
2023-05-31 16:36:11 +0200

    Write the fibs method

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
+def fibs(n)
+  i = 0
+  solutionArray = Array.new
+  if i == 0
+    solutionArray << 0
+    i += 1
+  end
+  if i == 1
+    solutionArray << 1
+    i += 1
+  end
+  while solutionArray.length < n
+    solutionArray << solutionArray[-1] + solutionArray[-2]
+    i += 1
+  end
+  return solutionArray
+end
 
+puts fibs(12)
